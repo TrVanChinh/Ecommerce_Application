f20ba608c106384863ced129a76e0f35e3d4a423
= <tuongdang297@gmail.com>
2024-05-28 16:13:43 +0700

    fix seller button Da giao hang

--- Ecommerce_app/Localhost.js
+++ Ecommerce_app/Localhost.js
@@ -1,4 +1,4 @@
 //export const API_BASE_URL = "http://10.0.2.2:3000";  
-export const API_BASE_URL = "http://192.168.1.102:3000"; 
+export const API_BASE_URL = "http://192.168.1.105:3000"; 
 //export const API_BASE_URL = "http://192.168.1.5:3000"; 
 // export const API_BASE_URL = "http://192.168.1.18:3000"; 
